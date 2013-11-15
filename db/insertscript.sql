43e39266d2990dc387138a2a359565a00e291a40
alex <alexolsen03@gmail.com>
2013-11-14 19:54:59 -0600

    click to vid

--- db/insertscript.sql
+++ db/insertscript.sql
@@ -3,6 +3,11 @@ insert into clips values ('2','1','beg title2', 'this is a beginner videeo desc'
 insert into clips values ('3','1','beg title3', 'this is a beginner videeo desc', '','','//www.youtube.com/embed/9v38mVkUA_M','assets/subs/begtitle1/eng.srt','assets/subs/begtitle1/kor.srt');
 insert into clips values ('4','2','itermediate title', 'this is an intermediate videeo desc', '','','//www.youtube.com/embed/9v38mVkUA_M','assets/subs/inttitle1/eng.srt','assets/subs/inttitle1/kor.srt');
 insert into clips values ('5','3','advanced title','this is an advanced video desc','','','//www.youtube.com/embed/9v38mVkUA_M','assets/subs/advtitle1/eng.srt','assets/subs/advtitle1/kor.srt');
+insert into clips values ('6','1','beg title4', 'this is a beginner videeo desc blah blah blah blah blah blah blahbalhblah blahba blabhal balhbbl bhalbalhba', '','','//www.youtube.com/embed/9v38mVkUA_M','assets/subs/begtitle1/eng.srt','assets/subs/begtitle1/kor.srt');
+insert into clips values ('7','1','beg title4', 'this is a beginner videeo desc blah blah blah blah blah blah blahbalhblah blahba blabhal balhbbl bhalbalhba', '','','//www.youtube.com/embed/9v38mVkUA_M','assets/subs/begtitle1/eng.srt','assets/subs/begtitle1/kor.srt');
+insert into clips values ('8','1','beg title4', 'this is a beginner videeo desc blah blah blah blah blah blah blahbalhblah blahba blabhal balhbbl bhalbalhba blabhal balhbbl bhalbalhba ', '','','//www.youtube.com/embed/9v38mVkUA_M','assets/subs/begtitle1/eng.srt','assets/subs/begtitle1/kor.srt');
+insert into clips values ('9','1','beg title4', 'this is a beginner videeo desc blah blah blah blah blah blah blahbalhblah blahba blabhal balhbbl bhalbalhbathis is a beginner videeo desc blah blah blah blah blah blah blahbalhblah blahba blabhal balhbbl bhalbalhba', '','','//www.youtube.com/embed/9v38mVkUA_M','assets/subs/begtitle1/eng.srt','assets/subs/begtitle1/kor.srt');
+
 
 insert into category_maps values ('1','1','1','','');
 insert into category_maps values ('2','1','2','','');
@@ -10,6 +15,14 @@ insert into category_maps values ('3','2','1','','');
 insert into category_maps values ('4','2','2','','');
 insert into category_maps values ('5','3','1','','');
 insert into category_maps values ('6','3','2','','');
+insert into category_maps values ('12','6','1','','');
+insert into category_maps values ('13','6','2','','');
+insert into category_maps values ('14','7','1','','');
+insert into category_maps values ('15','7','2','','');
+insert into category_maps values ('16','8','1','','');
+insert into category_maps values ('17','8','2','','');
+insert into category_maps values ('18','9','1','','');
+insert into category_maps values ('19','9','2','','');
 
 insert into category_maps values ('7','4','4','','');
 insert into category_maps values ('8','4','5','','');
