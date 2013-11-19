797bdc4e17940753692c5374490c44470a0d20c2
alex <alexolsen03@gmail.com>
2013-11-18 20:42:12 -0600

    seeds added

---
+++ db/insertscriptPostGres.sql
@@ -0,0 +1,40 @@
+insert into clips values ('1','1','beg title1', 'this is a beginner videeo desc', DEFAULT,DEFAULT,'//www.youtube.com/embed/9v38mVkUA_M','assets/subs/begtitle1/eng.srt','assets/subs/begtitle1/kor.srt');
+insert into clips values ('2','1','beg title2', 'this is a beginner videeo desc', DEFAULT,DEFAULT,'//www.youtube.com/embed/HOlgYV1o-xY','assets/subs/begtitle1/eng.srt','assets/subs/begtitle1/kor.srt');
+insert into clips values ('3','1','beg title3', 'this is a beginner videeo desc', DEFAULT,DEFAULT,'//www.youtube.com/embed/P98UaWPQrrM','assets/subs/begtitle1/eng.srt','assets/subs/begtitle1/kor.srt');
+insert into clips values ('4','2','itermediate title', 'this is an intermediate videeo desc', DEFAULT,DEFAULT,'//www.youtube.com/embed/9v38mVkUA_M','assets/subs/inttitle1/eng.srt','assets/subs/inttitle1/kor.srt');
+insert into clips values ('5','3','advanced title','this is an advanced video desc',DEFAULT,DEFAULT,'//www.youtube.com/embed/9v38mVkUA_M','assets/subs/advtitle1/eng.srt','assets/subs/advtitle1/kor.srt');
+insert into clips values ('6','1','beg title4', 'this is a beginner videeo desc blah blah blah blah blah blah blahbalhblah blahba blabhal balhbbl bhalbalhba', DEFAULT,DEFAULT,'//www.youtube.com/embed/FyL-Rzj_mUA','assets/subs/begtitle1/eng.srt','assets/subs/begtitle1/kor.srt');
+insert into clips values ('7','1','beg title4', 'this is a beginner videeo desc blah blah blah blah blah blah blahbalhblah blahba blabhal balhbbl bhalbalhba', DEFAULT,DEFAULT,'//www.youtube.com/embed/DMUMWhJWQOE','assets/subs/begtitle1/eng.srt','assets/subs/begtitle1/kor.srt');
+insert into clips values ('8','1','beg title4', 'this is a beginner videeo desc blah blah blah blah blah blah blahbalhblah blahba blabhal balhbbl bhalbalhba blabhal balhbbl bhalbalhba ', DEFAULT,DEFAULT,'//www.youtube.com/embed/bAHuysKAkDk','assets/subs/begtitle1/eng.srt','assets/subs/begtitle1/kor.srt');
+insert into clips values ('9','1','beg title4', 'this is a beginner videeo desc blah blah blah blah blah blah blahbalhblah blahba blabhal balhbbl bhalbalhbathis is a beginner videeo desc blah blah blah blah blah blah blahbalhblah blahba blabhal balhbbl bhalbalhba', DEFAULT,DEFAULT,'//www.youtube.com/embed/Q1U8MtM9DyA','assets/subs/begtitle1/eng.srt','assets/subs/begtitle1/kor.srt');
+
+
+insert into category_maps values ('1','1','1',DEFAULT,DEFAULT);
+insert into category_maps values ('2','1','2',DEFAULT,DEFAULT);
+insert into category_maps values ('3','2','1',DEFAULT,DEFAULT);
+insert into category_maps values ('4','2','2',DEFAULT,DEFAULT);
+insert into category_maps values ('5','3','1',DEFAULT,DEFAULT);
+insert into category_maps values ('6','3','2',DEFAULT,DEFAULT);
+insert into category_maps values ('12','6','1',DEFAULT,DEFAULT);
+insert into category_maps values ('13','6','2',DEFAULT,DEFAULT);
+insert into category_maps values ('14','7','1',DEFAULT,DEFAULT);
+insert into category_maps values ('15','7','2',DEFAULT,DEFAULT);
+insert into category_maps values ('16','8','1',DEFAULT,DEFAULT);
+insert into category_maps values ('17','8','2',DEFAULT,DEFAULT);
+insert into category_maps values ('18','9','1',DEFAULT,DEFAULT);
+insert into category_maps values ('19','9','2',DEFAULT,DEFAULT);
+
+insert into category_maps values ('7','4','4',DEFAULT,DEFAULT);
+insert into category_maps values ('8','4','5',DEFAULT,DEFAULT);
+
+insert into category_maps values ('10','5','4',DEFAULT,DEFAULT);
+insert into category_maps values ('11','5','5',DEFAULT,DEFAULT);
+
+
+
+insert into categories values ('1','food', '/assets/cat-icons/burger-y.png', 'assets/images/cat-icons/burger-b.png', DEFAULT, DEFAULT);
+insert into categories values ('2','transportation', '/assets/cat-icons/bus-y.png', 'assets/images/cat-icons/bus-b.png', DEFAULT, DEFAULT);
+insert into categories values ('3','shopping', '/assets/cat-icons/cart-y.png', 'assets/images/cat-icons/cart-b.png', DEFAULT, DEFAULT);
+insert into categories values ('4','relationships', '/assets/cat-icons/heart-y.png', 'assets/images/cat-icons/heart-b.png', DEFAULT, DEFAULT);
+insert into categories values ('5','telephone', '/assets/cat-icons/phone-y.png', 'assets/images/cat-icons/phone-b.png', DEFAULT, DEFAULT);
+insert into categories values ('6','conversation', '/assets/cat-icons/speech-y.png', 'assets/images/cat-icons/speech-b.png', DEFAULT, DEFAULT);
